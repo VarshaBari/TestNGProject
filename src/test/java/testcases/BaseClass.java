a46b2f727d50e40b496263f731176c3ab413089d
VarshaK <kaplevarsha8@gmail.com>
2024-10-20 17:11:38 +0000

    changes done

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -28,81 +28,67 @@ public class BaseClass {
 	public static WebDriver driver;
 	public XSSFWorkbook wbook;
 	public XSSFSheet sheet;
-	
-	
+
 	private ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
 	public void SetDriver(WebDriver driver) {
 		threadLocalDriver.set(driver);
 	}
-	
+
 	public WebDriver GetDriver() {
 
 		return threadLocalDriver.get();
 
 	}
 
-
-	@BeforeMethod(alwaysRun=true)
+	@BeforeMethod(alwaysRun = true)
 	public void setUpDriver() throws MalformedURLException {
-		
+
 //String browser = System.getProperty("Browser");
-String browser = "chrome";
+		String browser = "chrome";
 
 		if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.chromedriver().clearDriverCache().setup();
 			driver = new FirefoxDriver();
-		}else if(browser.equalsIgnoreCase("remote-chrome")) {
-			
+		} else if (browser.equalsIgnoreCase("remote-chrome")) {
+
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setPlatform(Platform.LINUX);
 			cap.setBrowserName("chrome");
 
 			URL hub = new URL("http://localhost:4444/");
 			driver = new RemoteWebDriver(hub, cap);
-			
-		}
-		else {
+
+		} else {
+			WebDriverManager.chromedriver().clearDriverCache().setup();
 			driver = new ChromeDriver();
 		}
-		
+
 		SetDriver(driver);
 		GetDriver().get("https://simplilearn.com/");
 		GetDriver().manage().window().maximize();
 		GetDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+	
 		
-
-
-		WebDriverManager.chromedriver().clearDriverCache().setup();
-		driver = new ChromeDriver();
-		driver.get("https://www.simplilearn.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 	}
 
-
-	@AfterMethod(alwaysRun=true)
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		GetDriver().quit();
 	}
 
 	@BeforeTest
 	public void SetupExcel() throws IOException {
-		
-		
-	
-		
+
 	}
-	
+
 	@AfterTest
 	public void CloseExcel() throws IOException {
-		
-		//wbook.close();
-		
-	}
+
+		// wbook.close();
 
 	}
 
-
-
+}
